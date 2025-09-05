252cfdd22018e3f694feceeef89c28b88542467d
gamuen <136949922+gamuen@users.noreply.github.com>
2025-09-05 16:19:43 +0900

    Add files via upload

---
+++ 6~12장 복습 예제들/6장 복습/script1.js
@@ -0,0 +1,6 @@
+function over(obj) {
+    obj.src="joseph-gonzalez-zcUgjyqEwe8-unsplash.jpg";
+    }
+    function out(obj) {
+    obj.src="Mouthwatering Cheesy Garlic Bread Recipe_ Easy & Delicious!.jpg";
+    }
